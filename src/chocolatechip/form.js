59463ff6578514df443d058ee9637b71628b1658
Nick Simmons <nick.simmons@jadedpixel.com>
2014-11-06 15:14:17 -0500

    Add form2FormData + form tests

--- src/chocolatechip/form.js
+++ src/chocolatechip/form.js
@@ -1,6 +1,60 @@
 (function(){
   "use strict";
-  $.extend($, {  
+  function getFormValues(rootNode) {
+    var result = [];
+    var currentNode = rootNode.firstChild;
+    while (currentNode) {
+      if (currentNode.nodeName.match(/INPUT|SELECT|TEXTAREA/i)) {
+        result.push({ name: currentNode.name, value: getFieldValue(currentNode)});
+      } else {
+        var subresult = getFormValues(currentNode);
+        result = result.concat(subresult);
+      }
+      currentNode = currentNode.nextSibling;
+    }
+    return result;
+  }
+  function getFieldValue(fieldNode) {
+    if (fieldNode.nodeName === 'INPUT') {
+      if (fieldNode.type.toLowerCase() === 'radio' || fieldNode.type.toLowerCase() === 'checkbox') {
+        if (fieldNode.checked) {
+          return fieldNode.value;
+        }
+      } else if (fieldNode.type.toLowerCase() === 'file' && fieldNode.files.length > 0) {
+        return fieldNode.files[0];
+      } else {
+        if (!fieldNode.type.toLowerCase().match(/button|reset|submit|image/i)) {
+          return fieldNode.value;
+        }
+      }
+    } else {
+      if (fieldNode.nodeName === 'TEXTAREA') {
+        return fieldNode.value;
+      } else {
+        if (fieldNode.nodeName === 'SELECT') {
+          return getSelectedOptionValue(fieldNode);
+        }
+      }
+    }
+    return '';
+  }
+  function getSelectedOptionValue(selectNode) {
+    var multiple = selectNode.multiple;
+    if (!multiple) {
+      return selectNode.value;
+    }
+    if (selectNode.selectedIndex > -1) {
+      var result = [];
+      $('option', selectNode).each(function(item) {
+        if (item.selected) {
+          result.push(item.value);
+        }
+      });
+      return result;
+    }
+  }
+
+  $.extend($, {
     // Convert form values into JSON object:
     form2JSON : function(rootNode, delimiter) {
       rootNode = typeof rootNode === 'string' ? $(rootNode)[0] : rootNode;
@@ -8,58 +62,7 @@
       var formValues = getFormValues(rootNode);
       var result = {};
       var arrays = {};
-      
-      function getFormValues(rootNode) {
-        var result = [];
-        var currentNode = rootNode.firstChild;
-        while (currentNode) {
-          if (currentNode.nodeName.match(/INPUT|SELECT|TEXTAREA/i)) {
-            result.push({ name: currentNode.name, value: getFieldValue(currentNode)});
-          } else {
-            var subresult = getFormValues(currentNode);
-            result = result.concat(subresult);
-          }
-          currentNode = currentNode.nextSibling;
-        }
-        return result;
-      }
-      function getFieldValue(fieldNode) {
-        if (fieldNode.nodeName === 'INPUT') {
-          if (fieldNode.type.toLowerCase() === 'radio' || fieldNode.type.toLowerCase() === 'checkbox') {
-            if (fieldNode.checked) {
-              return fieldNode.value;
-            }
-          } else {
-            if (!fieldNode.type.toLowerCase().match(/button|reset|submit|image/i)) {
-              return fieldNode.value;
-            }
-          }
-        } else {
-          if (fieldNode.nodeName === 'TEXTAREA') {
-            return fieldNode.value;
-          } else {
-            if (fieldNode.nodeName === 'SELECT') {
-              return getSelectedOptionValue(fieldNode);
-            }
-          }
-        }
-        return '';
-      }
-      function getSelectedOptionValue(selectNode) {
-        var multiple = selectNode.multiple;
-        if (!multiple) {
-          return selectNode.value;
-        }
-        if (selectNode.selectedIndex > -1) {
-          var result = [];
-          $('option', selectNode).each(function(item) {
-            if (item.selected) {
-              result.push(item.value);
-            }
-          });
-          return result;
-        }
-      }   
+
       formValues.each(function(item) {
         var value = item.value;
         if (value !== '') {
@@ -90,13 +93,13 @@
                 } else {
                   if (!arrays[arrName][arrIdx]) {
                     currResult[arrName].push({});
-                    arrays[arrName][arrIdx] = 
+                    arrays[arrName][arrIdx] =
                     currResult[arrName][currResult[arrName].length - 1];
                   }
                 }
                 currResult = arrays[arrName][arrIdx];
               } else {
-                if (j < nameParts.length - 1) { 
+                if (j < nameParts.length - 1) {
                   if (!currResult[namePart]) {
                     currResult[namePart] = {};
                   }
@@ -110,6 +113,18 @@
         }
       });
       return result;
+    },
+    // Convert form values into a FormData object:
+    form2FormData : function(rootNode) {
+      rootNode = typeof rootNode === 'string' ? $(rootNode)[0] : rootNode;
+      var formData = new FormData();
+      var formValues = getFormValues(rootNode);
+
+      formValues.each(function(item) {
+        if (item.value !== '')
+          formData.append(item.name, item.value);
+      });
+      return formData;
     }
   });
 })();
